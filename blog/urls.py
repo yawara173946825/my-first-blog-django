4d9b8008967c4e24008a78774da04cf0eacd2275
Yawara Miyahara <yawara173946825@users.noreply.github.com>
2024-02-16 17:36:23 +0900

    コメント、リプライ機能実装

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from blog.views import IndexView, PostDetailView, CategoryListView, TagListView, CategoryPostView, TagPostView,SearchPostView
+from blog.views import IndexView, PostDetailView, CategoryListView, TagListView, CategoryPostView, TagPostView,SearchPostView,CommentFormView,comment_approve,comment_remove, ReplyFormView,reply_approve,reply_remove
 
 app_name = 'blog'
 
@@ -13,4 +13,10 @@ urlpatterns = [
          CategoryPostView.as_view(), name='category_post'),
     path('tag/<str:tag_slug>/', TagPostView.as_view(), name='tag_post'),
     path('search/', SearchPostView.as_view(), name='search_post'),
+    path('comment/<int:pk>/', CommentFormView.as_view(), name='comment_form'),
+    path('comment/<int:pk>/approve/', comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/comment_remove/', comment_remove, name='comment_remove'),
+    path('reply/<int:pk>/', ReplyFormView.as_view(), name='reply_form'),
+    path('reply/<int:pk>/approve/', reply_approve, name='reply_approve'),
+    path('reply/<int:pk>/remove/',reply_remove, name='reply_remove' ),
 ]
